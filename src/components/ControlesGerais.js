bc0796d261f8475821d1593380e49968ce5adf38
Guilherme Orige Kernbichler <guilhermeokern@gmail.com>
2026-08-26 19:30:24 -0300

    feat: adiciona componente de desfazer jogada

--- src/components/ControlesGerais.js
+++ src/components/ControlesGerais.js
@@ -1,7 +1,8 @@
-export default function ControlesGerais({ onReiniciar}) {
+export default function ControlesGerais({ onReiniciar, onDesfazerJogada }) {
     return (
         <div>
             <button onClick={() => onReiniciar()}>Reiniciar Partida</button>
+            <button onClick={() => onDesfazerJogada()}>Desfazer Jogada</button>
         </div>
     );
 }
